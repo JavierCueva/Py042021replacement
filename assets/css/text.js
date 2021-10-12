5d9d431276e92d2d0261e4eb92aaca8197677955
Javier Cueva <s.javier.cueva@gmail.com>
2021-10-11 19:14:55 -0500

    added support pages

---
+++ assets/css/text.js
@@ -0,0 +1,4 @@
+//prompt("dame tu nombre");
+
+const welcomeText = "We are committed to provide the best service to your heavy machinery, as much as the most efficient advisory to purchase new or semi-new equipment";
+console.log(welcomeText);
---
+++ directory-names.js
@@ -0,0 +1,22 @@
+//alert("WIP");
+
+const juanG = {
+
+    name: "Juan",
+    ph: "33 - 22 - 44 - 55",
+    email: "juan@juan.com",
+    location: "Guadalajara"
+}
+
+const javierC = {
+    name: "Javier",
+    ph: "33 - 10 - 30 - 20",
+    email: "javier@javier.com",
+    location: "Guadalajara"
+}
+
+//console.log(juanG, javierC)
+
+document.write(javierC.name + javierC.ph + " ");
+document.writeln(javierC.name + javierC.ph)
+console.log(javierC.name + " " + javierC.ph)
